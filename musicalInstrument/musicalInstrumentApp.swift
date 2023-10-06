4b9339383da45ea530077ff68787a53e0de6fc13
Miyuki <87884079+miyuki-KK@users.noreply.github.com>
2023-10-07 00:09:38 +0900

    Initial Commit

---
+++ musicalInstrument/musicalInstrumentApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicalInstrumentApp.swift
+//  musicalInstrument
+//
+//  Created by Miyuki Kikuchi on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct musicalInstrumentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
